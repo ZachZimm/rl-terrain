e9d22f510f0ed373733ea9c3a00b97a33820e3a9
Zach Zimmermann <zach@jzimm.com>
2024-10-26 18:52:11 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,247 @@
+package main
+
+import (
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/aquilax/go-perlin"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+const (
+	tileSizeX = 1
+	tileSizeY = 1
+)
+
+var (
+	windowWidth  = 1200
+	windowHeight = 675
+	numTilesX    = windowWidth / tileSizeX
+	numTilesY    = windowHeight / tileSizeY
+
+	tileMap [1200][675]float64
+	lehmer  *Lehmer
+)
+
+func initTileMap() {
+	for i := 0; i < numTilesX; i++ {
+		for j := 0; j < numTilesY; j++ {
+			tileMap[i][j] = 0.5
+		}
+	}
+}
+
+func normalizeTileMap() {
+	minValue := 1.0
+	maxValue := 0.0
+	for i := 0; i < numTilesX; i++ {
+		for j := 0; j < numTilesY; j++ {
+			minValue = math.Min(minValue, tileMap[i][j])
+			maxValue = math.Max(maxValue, tileMap[i][j])
+		}
+	}
+
+	for i := 0; i < numTilesX; i++ {
+		for j := 0; j < numTilesY; j++ {
+			tileMap[i][j] = (tileMap[i][j] - minValue) / (maxValue - minValue)
+		}
+	}
+}
+
+func setTilesRandomly_rl(willModify bool) {
+	for i := 0; i < numTilesX; i++ {
+		for j := 0; j < numTilesY; j++ {
+			randValue := float64(rl.GetRandomValue(0, 100)) / 100.0
+			if willModify {
+				oldValue := tileMap[i][j]
+				newValue := oldValue * randValue
+				tileMap[i][j] = (oldValue + newValue) / 2
+			} else {
+				tileMap[i][j] = randValue
+			}
+		}
+	}
+}
+
+func setTilesRandomly_rand(willModify bool) {
+	for i := 0; i < numTilesX; i++ {
+		for j := 0; j < numTilesY; j++ {
+			randValue := rand.Float64()
+			if willModify {
+				oldValue := tileMap[i][j]
+				newValue := oldValue * randValue
+				tileMap[i][j] = (oldValue + newValue) / 2
+			} else {
+				tileMap[i][j] = randValue
+			}
+		}
+	}
+}
+
+var (
+	alpha       = 3.
+	beta        = 4.
+	n     int32 = 10
+	seed  int64 = 100
+)
+
+func setTilesRandomly_perlin(willModify bool) {
+	seed = rand.Int63()
+	p := perlin.NewPerlin(alpha, beta, n, seed)
+	for x := 0; x < numTilesX; x++ {
+		for y := 0; y < numTilesY; y++ {
+			randValue := p.Noise2D(float64(x)/float64(numTilesX), float64(y)/float64(numTilesY))
+			randValue = math.Min(math.Max(randValue, 0), 1)
+			if willModify {
+				oldValue := tileMap[x][y]
+				newValue := (oldValue * randValue) + 0.25
+				newValue = math.Min(math.Max(newValue, 0), 1)
+				tileMap[x][y] = (oldValue + newValue) / 2
+			} else {
+				tileMap[x][y] = randValue
+			}
+		}
+	}
+}
+
+func setTilesRandomly_lehmer(willModify bool) {
+	seed = lehmer.Int63()
+	for x := 0; x < numTilesX; x++ {
+		for y := 0; y < numTilesY; y++ {
+			randInt := lehmer.Int63() % 100
+			randValue := float64(randInt) / 100.0
+			randValue = math.Min(math.Max(randValue, 0), 1)
+			if willModify {
+				oldValue := tileMap[x][y]
+				newValue := (oldValue * randValue) + 0.25
+				newValue = math.Min(math.Max(newValue, 0), 1)
+				tileMap[x][y] = (oldValue + newValue) / 2
+			} else {
+				tileMap[x][y] = randValue
+			}
+		}
+	}
+}
+
+func mapGenerationRoutine(willReset bool) {
+	setTilesRandomly_lehmer(!willReset)
+	lehmerSteps := 2
+	for i := 0; i < lehmerSteps; i++ {
+		setTilesRandomly_lehmer(true)
+	}
+	perlinSteps := 8
+	for i := 0; i < perlinSteps; i++ {
+		setTilesRandomly_perlin(true)
+	}
+	normalizeTileMap()
+}
+
+func getColorFromTile(tile float64) rl.Color {
+	return rl.NewColor(0, uint8(tile*255), 0, 255)
+}
+
+func getColorFromTile_hsl(tile float64) rl.Color {
+	hue := tile * 360
+	return rl.ColorFromHSV(float32(hue), 0.5, 0.75)
+}
+
+func main() {
+	lehmer = NewLehmer(rand.Int63())
+	initTileMap()
+	const keyPressTimer = 2
+	lastKeyPress := "1"
+	lastKeyPressTime := time.Now()
+	showLastKeyPress := true
+
+	rl.InitWindow(int32(windowWidth), int32(windowHeight), "Tilemap")
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
+
+		for i := 0; i < numTilesX; i++ {
+			for j := 0; j < numTilesY; j++ {
+				rl.DrawRectangle(int32(i*tileSizeX), int32(j*tileSizeY), int32(tileSizeX), int32(tileSizeY), getColorFromTile_hsl(tileMap[i][j]))
+			}
+		}
+		rl.DrawRectangle(5, int32(float32(windowHeight)*0.91), 320, 30, rl.Fade(rl.White, 0.5))
+		if showLastKeyPress && time.Since(lastKeyPressTime).Seconds() < keyPressTimer {
+			rl.DrawText("Last key pressed: "+lastKeyPress, 10, int32(float32(windowHeight)*0.92), 20, rl.Black)
+		}
+
+		rl.DrawRectangle(5, 5, 500, 170, rl.Fade(rl.White, 0.5))
+		// Display the controls on the screen
+		rl.DrawText("Press R to randomize (raylib)", 10, 10, 20, rl.Black)
+		rl.DrawText("Press M to randomize (rand)", 10, 30, 20, rl.Black)
+		rl.DrawText("Press P to randomize (perlin)", 10, 50, 20, rl.Black)
+		rl.DrawText("Press L to randomize (lehmer)", 10, 70, 20, rl.Black)
+		rl.DrawText("Press N to normalize", 10, 90, 20, rl.Black)
+		rl.DrawText("Press G to generate with a layered approach", 10, 110, 20, rl.Black)
+		rl.DrawText("Hold shift to layer (multiply) the random values", 10, 130, 20, rl.Black)
+		rl.DrawText("Press 1 to reset", 10, 150, 20, rl.Black)
+
+		rl.EndDrawing()
+		if rl.IsKeyPressed(rl.KeyR) {
+			lastKeyPress = "Shift + R"
+			willModify := true
+			if !(rl.IsKeyDown(rl.KeyLeftShift) || rl.IsKeyDown(rl.KeyRightShift)) {
+				lastKeyPress = "R"
+				initTileMap()
+				willModify = false
+			}
+			setTilesRandomly_rl(willModify)
+			lastKeyPressTime = time.Now()
+		} else if rl.IsKeyPressed(rl.KeyM) {
+			lastKeyPress = "Shift + M"
+			willModify := true
+			if !(rl.IsKeyDown(rl.KeyLeftShift) || rl.IsKeyDown(rl.KeyRightShift)) {
+				lastKeyPress = "M"
+				initTileMap()
+				willModify = false
+			}
+			setTilesRandomly_rand(willModify)
+			lastKeyPressTime = time.Now()
+		} else if rl.IsKeyPressed(rl.KeyP) {
+			lastKeyPress = "Shift + P"
+			willModify := true
+			if !(rl.IsKeyDown(rl.KeyLeftShift) || rl.IsKeyDown(rl.KeyRightShift)) {
+				lastKeyPress = "P"
+				initTileMap()
+				willModify = false
+			}
+			setTilesRandomly_perlin(willModify)
+			lastKeyPressTime = time.Now()
+		} else if rl.IsKeyPressed(rl.KeyL) {
+			lastKeyPress = "Shift + L"
+			willModify := true
+			if !(rl.IsKeyDown(rl.KeyLeftShift) || rl.IsKeyDown(rl.KeyRightShift)) {
+				lastKeyPress = "L"
+				initTileMap()
+				willModify = false
+			}
+			setTilesRandomly_lehmer(willModify)
+			lastKeyPressTime = time.Now()
+		} else if rl.IsKeyPressed(rl.KeyN) {
+			lastKeyPress = "N"
+			normalizeTileMap()
+			lastKeyPressTime = time.Now()
+		} else if rl.IsKeyPressed(rl.KeyOne) {
+			lastKeyPress = "1"
+			initTileMap()
+			lastKeyPressTime = time.Now()
+		} else if rl.IsKeyPressed(rl.KeyG) {
+			lastKeyPress = "G"
+			willReset := true
+			if rl.IsKeyDown(rl.KeyLeftShift) || rl.IsKeyDown(rl.KeyRightShift) {
+				lastKeyPress = "Shift + G"
+				willReset = false
+			}
+			go mapGenerationRoutine(willReset)
+			lastKeyPressTime = time.Now()
+		}
+
+	}
+	rl.CloseWindow()
+}
